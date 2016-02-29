23d1176143d18f91c9e68ebf4cfe37123e76f1a1
Anthelios <julio.caruncho.a@gmail.com>
2016-02-29 20:06:42 -0300

    Mallas

--- Views/templates/mallas/ver_malla.php
+++ Views/templates/mallas/ver_malla.php
@@ -12,17 +12,16 @@
       <img src="<?php echo RESOURCES_DIR.'img/hola.jpg';?>">
       <span class="card-title"><strong><h4><a href="javascript:history.go(-1)"><i class="material-icons small white-text left" style="font-size: 40px">arrow_back</i></a>Malla</h4></strong></span>
     </div>
-  <div class="card-content">
+  <div class="card-content center">
   <?php
     foreach ($this->data['niveles'] as $niveles) {
       echo '<div class="semestre">';
-      echo '<h1>Semestre</h1>';
+      echo '<h1>Semestre '.$niveles[0]->getNivel().'</h1>';
       foreach ($niveles as $nivel) {
-        echo '<div class="materia CB" id="'.$nivel->getCodigo().'">';
-        echo '<header>'.$nivel->getCodigo().'</header>';
-        echo '<section>'.$nivel->getNombre().'</section>';
-        echo '</div>';
-
+            echo '<div class="materia CB" id="'.$nivel->getCodigo().'">';
+            echo '<header>'.$nivel->getCodigo().'</header>';
+            echo '<section>'.$nivel->getNombre().'</section>';
+            echo '</div>';
         }
         echo '</div>';
     } ?>      
